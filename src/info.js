e516ee56a059240da0245e0c32184b57140f967b
AUDREY KADJAR <audreykadjar@gmail.com>
2020-07-21 22:04:54 +0200

    save edits

--- src/info.js
+++ src/info.js
@@ -9,8 +9,8 @@ function Info() {
                 You love an image's mood and would like to know its colors? you
                 are designing a product and need some inspiration for the color
                 scheme? you are at the right place! This website enables you to
-                detect colors in images and to save and categorize color
-                palettes. <br /> <br />
+                detect colors in images and save and categorize color palettes.
+                <br /> <br />
                 GET STARTED 🎨 <br />1 - upload an image to the&nbsp;
                 <Link className="underline" to="/spotter">
                     color spotter&nbsp;
